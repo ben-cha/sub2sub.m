90161b0c5ff303e2d72cbb4a38a9c80993f6b05f
Ben <41757454+ben-cha@users.noreply.github.com>
2023-03-26 12:44:40 -0400

    Initial commit

---
+++ sub2sub.m
@@ -0,0 +1,22 @@
+function [row, col] = sub2sub(szOrig, szSubdiv, r, c, p)
+
+%{
+szOrig     = size of the original matrix before subdividing
+szSubdiv   = size of the subdivision
+r          = row index in the subdivision
+c          = column index in subdivision
+p          = page index of the subdivision or linear index of the cell that contains the subdivisions
+%}
+
+nrGrid = szOrig(1)./szSubdiv(1); % number of rows in the resultant grid of subdivisions
+% ncGrid = szOrig(2)/szSubdiv(2);
+
+nrSubdiv = szSubdiv(1); % number of rows in each subdivision
+ncSubdiv = szSubdiv(2); % number of columns in each subdivision
+
+col = floor( ( p - 1) ./ nrGrid ) .* ncSubdiv + c; % respective column index of the original matrix
+row = mod( p - 1 , nrGrid) .* nrSubdiv + r;        % respective row index of the original matrix
+
+end
+
+
